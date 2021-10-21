5d35de88dff07b6269dcfddb792e902ebb0232fd
fabian4 <fabian4@163.com>
2021-10-21 22:05:56 +0800

    update views

--- app/src/main/java/com/fabian/fyi/MainActivity.java
+++ app/src/main/java/com/fabian/fyi/MainActivity.java
@@ -2,7 +2,12 @@ package com.fabian.fyi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.clientId);
+        textView.setText("啊啊啊");
+
+        textView.setOnClickListener(v -> {
+            ClipboardManager clipboard = (ClipboardManager) getApplicationContext().getSystemService(CLIPBOARD_SERVICE);
+            String text = textView.getText().toString();
+            ClipData myClip = ClipData.newPlainText("text", text);
+            clipboard.setPrimaryClip(myClip);
+            Toast.makeText(getApplicationContext(), text + " 已复制", Toast.LENGTH_SHORT).show();
+        });
     }
 }
